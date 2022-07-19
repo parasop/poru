2444a5cbb9c48ed6bc8bdde88bf1e3e6b6aefb49
parasop <63485824+parasop@users.noreply.github.com>
2022-07-19 16:02:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
+
 module.exports = {
     Node: require("./src/Node"),
     Player: require("./src/Player"),
-    Poru: require("./src/Poru")
+    Poru: require("./src/Poru"),
+    Spotify : require("./src/platform/Spotify"),
+    Deezer : require("./src/platform/Deezer"),
+    AppleMusic : require("./src/platform/AppleMusic"),
+    Queue : require("./src/guild/Queue"),
+    Filter : require("./src/guild/Filter"),
+    Track: require("./src/guild/Track"),
+    PoruTrack : require("./src/guild/PoruTrack")
+    
+    
+
 }
-process.on('unhandledRejection', error => {
-    if (error.code === '10008' || error.code === '10062') return;
-    console.log(error.stack);
-  });
-  process.on('uncaughtException', (err, origin) => {
-    console.log(err, origin);
-  });
-  
-  process.on('uncaughtExceptionMonitor', (err, origin) => {
-    console.log(err, origin);
-  });
-  
-  process.on('multipleResolves', (type, promise, reason) => {
-    console.log(type, promise, reason);
-  });
