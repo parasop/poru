756371b5c0004f832b4c6ec9af3319f4c1690d85
parasop <63485824+parasop@users.noreply.github.com>
2022-11-08 19:49:30 +0530

    Update Response.js

--- src/guild/Response.js
+++ src/guild/Response.js
@@ -2,9 +2,9 @@ const Track = require("./Track");
 
 class Response {
   constructor(data) {
-    this.tracks = data.tracks.map((track) => new Track(track));
-    this.loadType = data.loadType;
-    this.playlistInfo = data.playlistInfo;
+    this.tracks = data?.tracks?.map((track) => new Track(track));
+    this.loadType = data?.loadType;
+    this.playlistInfo = data?.playlistInfo;
   }
 }
 
