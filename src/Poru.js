1c09bd9b232f0e5456a0bd6a07fdb2a47863eaa7
Jack <90880784+BTX-ON-YT@users.noreply.github.com>
2022-12-09 07:22:38 +0000

    Fix eris ws error and JSON stringification error

--- src/Poru.js
+++ src/Poru.js
@@ -48,7 +48,7 @@ class Poru extends EventEmitter {
       });
 
     } else if (config.libraries.eris.includes(this.options.library)) {
-
+      
       this.sendData = (data) => {
         const guild = client.guilds.get(data.d.guild_id);
         if (guild) guild.shard.ws.send(JSON.stringify({ op: data?.op, d: data?.d}));
