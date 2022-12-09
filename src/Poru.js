3a33ff3d22736e52f1246c33e55390b022e3a07c
Jack <90880784+BTX-ON-YT@users.noreply.github.com>
2022-12-09 07:14:39 +0000

    Dev dont use

--- src/Poru.js
+++ src/Poru.js
@@ -50,7 +50,7 @@ class Poru extends EventEmitter {
     } else if (config.libraries.eris.includes(this.options.library)) {
 
       this.sendData = (data) => {
-        console.log(data);
+        console.log(JSON.stringify(data));
         const guild = client.guilds.get(data.d.guild_id);
         if (guild) guild.shard.ws.send(data?.op, data?.d);
       };
