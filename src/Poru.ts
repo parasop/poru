77c22ad6de71ab48593e065fca4769f2dde031ce
parasop <63485824+parasop@users.noreply.github.com>
2023-02-28 13:06:00 +0530

    Update Poru.ts

--- src/Poru.ts
+++ src/Poru.ts
@@ -195,7 +195,7 @@ export class Poru extends EventEmitter {
       case "oceanic": {
         this.send = (packet: any) => {
           const guild = client.guilds.get(packet.d.guild_id);
-          if (guild) guild.shard.sendWS(packet?.op, packet?.d);
+          if (guild) guild.shard.send(packet?.op, packet?.d);
         };
 
         client.on("packet", async (packet: any) => {
