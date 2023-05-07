594bdf0a017646b5af38808c34efde1dc557f576
parasop <63485824+parasop@users.noreply.github.com>
2023-05-07 23:39:09 +0530

    Update CustomFilters.ts

--- src/Player/CustomFilters.ts
+++ src/Player/CustomFilters.ts
@@ -13,7 +13,21 @@ export class customFilter extends Filters {
   constructor(player: Player) {
     super(player);
     this.player = player;
+    this.bassboost =0;
   }
+  
+  public setBassboost(val:number):this{
+    if(!this.player) return;
+    if(val < 0 && val > 6)  throw Error('bassboost value must be between 0 to 5')
+    this.bassboost = val;
+    let num = (val - 1) * (1.25 / 9) - 0.25;
+    this.setEqualizer(Array(13).fill(0).map((n, i) => ({
+      band: i,
+      gain: num
+  })))
+
+ return this;  
+}
 
   public setSlowmode(val: boolean): this {
     if (!this.player) return;
