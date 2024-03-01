d82a2c8fcbbfcc6956ca18a3110161b419fc37f1
UnschooledGamer <unschooledgamer@gmail.com>
2024-03-01 09:57:01 +0530

    fix(Readme): use github raw poru icon/logo link

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -6,7 +6,6 @@ import path from 'path';
 const config: Config = {
   title: 'Poru',
   tagline: 'A stable and powerful Lavalink client with some best features',
-  staticDirectories: ["assets", "static"],
   favicon: 'img/poru.png',
 
   // Set the production url of your site here
