cfde6a46e54226aabc35eb10fa4e7a720ea413e6
Joniii11 <jonasdimpfl@gmail.com>
2024-03-03 18:10:34 +0100

    Add types and interfaces for voice state update

--- src/Player/Connection.ts
+++ src/Player/Connection.ts
@@ -4,7 +4,39 @@ export interface IVoiceServer {
     token: string;
     sessionId: string;
     endpoint: string;
-}
+};
+
+type TYear = `${number}${number}${number}${number}`;
+type TMonth = `${number}${number}`;
+type TDay = `${number}${number}`;
+type THours = `${number}${number}`;
+type TMinutes = `${number}${number}`;
+type TSeconds = `${number}${number}`;
+type TMilliseconds = `${number}${number}${number}`;
+type TDateISODate = `${TYear}-${TMonth}-${TDay}`;
+type TDateISOTime = `${THours}:${TMinutes}:${TSeconds}.${TMilliseconds}`;
+
+type TDateISO = `${TDateISODate}T${TDateISOTime}Z`;
+
+/**
+ * Discord Voice State Update Types
+ * @reference https://discord.com/developers/docs/resources/voice#voice-state-object
+ */
+export interface SetStateUpdate {
+    guild_id?: string;
+    channel_id: string;
+    user_id: string;
+    member?: Record<string, unknown>
+    session_id: string;
+    deaf: boolean;
+    mute: boolean;
+    self_deaf: boolean;
+    self_mute: boolean;
+    self_stream?: boolean;
+    self_video: boolean;
+    suppress: boolean;
+    request_to_speak_timestamp?: TDateISO
+};
 
 /**
   * The connection class
@@ -43,7 +75,7 @@ export class Connection {
      * Set the voice server update
      * @param data The data from the voice server update
      */
-    public setServersUpdate(data) {
+    public setServersUpdate(data: IVoiceServer) {
         if (!data.endpoint) throw new Error("NO Session id found");
 
         this.voice.endpoint = data.endpoint;
@@ -61,13 +93,13 @@ export class Connection {
             this.player.node.name,
             `[Voice] <- [Discord] : Voice Server Update | Server: ${this.region} Guild: ${this.player.guildId}`
         );
-    }
+    };
 
     /**
      * Set the state update
      * @param data The data from the state update
      */
-    setStateUpdate(data) {
+    public setStateUpdate(data: SetStateUpdate) {
         const { session_id, channel_id, self_deaf, self_mute } = data;
         if (
             this.player.voiceChannel &&
@@ -80,6 +112,5 @@ export class Connection {
         this.self_deaf = self_deaf;
         this.self_mute = self_mute;
         this.voice.sessionId = session_id || null;
-        //  this.player.poru.emit('debug', this.player.node.name, `[Voice] <- [Discord] : State Update Received | Channel: ${this.player.voiceChannel} Session ID: ${session_id} Guild: ${this.player.guildId}`);
     }
 }
