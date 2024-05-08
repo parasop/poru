08d506b6515639d2cb819411f0ba118ee1716496
Joniii11 <jonasdimpfl@gmail.com>
2024-05-08 15:34:10 +0200

    Updated the header name to be correct

--- src/Node/Rest.ts
+++ src/Node/Rest.ts
@@ -150,7 +150,7 @@ export class Rest {
             Authorization: this.password,
         };
 
-        if (this.isNodeLink) headers["Content-Encoding"] = "brotli, gzip, deflate";
+        if (this.isNodeLink) headers["Accept-Encoding"] = "brotli, gzip, deflate";
 
         return headers;
     }
