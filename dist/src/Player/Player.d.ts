77c8ef828c3c6059b89f8ffc0af5316b5a58062e
parasop <63485824+parasop@users.noreply.github.com>
2024-11-27 12:54:37 +0000

    fix

--- dist/src/Player/Player.d.ts
+++ dist/src/Player/Player.d.ts
@@ -98,8 +98,6 @@ export declare class Player extends EventEmitter {
     deaf: boolean;
     /** The volume of the player (0-1000) */
     volume: number;
-    lastPlayTimestamp: any;
-    cooldown: any;
     /** Should only be used when the node is a NodeLink */
     protected voiceReceiverWsClient: WebSocket | null;
     protected isConnectToVoiceReceiver: boolean;
