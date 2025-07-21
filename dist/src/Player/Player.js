3739f13591f5d77a42f1a72ea19b84eb4fe8b7b0
parasop <makvanaparas564@gmail.com>
2025-07-22 03:18:04 +0530

    Added my feature or fix

--- dist/src/Player/Player.js
+++ dist/src/Player/Player.js
@@ -580,7 +580,7 @@ class Player extends events_1.EventEmitter {
                     });
                 }
                 this.poru.emit("socketClose", this, this.currentTrack, data);
-                await this.pause(true);
+                //    await this.pause(true);
                 this.poru.emit("debug", `Player -> ${this.guildId}`, "Player paused Cause Channel deleted Or Client was kicked");
                 break;
             }
